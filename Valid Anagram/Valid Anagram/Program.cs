54fa471143d5398871933bef0db5af3a59d2b397
Asmaa Gamal <gasmaa083@gmail.com>
2023-01-20 21:07:03 +0200

    Problem-2 Valid-Anagram solution using array&sort

---
+++ Valid Anagram/Valid Anagram/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Valid_Anagram
+{
+    internal class Program
+    {
+        public static bool  IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+                return false;
+
+            char[] ss = s.ToCharArray();
+            char[] tt = t.ToCharArray();
+            Array.Sort(ss);
+            Array.Sort(tt);
+            return ss.SequenceEqual(tt);
+        }
+        static void Main(string[] args)
+        {
+            String s , t;
+            s = Console.ReadLine();
+            t = Console.ReadLine();
+            Console.WriteLine(IsAnagram(s , t));
+        }
+    }
+}
