3e098bd25ffc91613cfdb29b6b86e74e0b2f4c9f
Asmaa Gamal <gasmaa083@gmail.com>
2023-01-20 20:54:52 +0200

    Problem 1 Contains Duplicate solution using HashSet

---
+++ Contains Duplicate/Contains Duplicate/Contains Duplicate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+
+namespace Contains_Duplicate
+{
+    internal class Program
+    { 
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> unique = new HashSet<int>(nums);
+            return nums.Length > unique.Count;
+        }
+    
+        static void Main(string[] args)
+        {
+            int size;
+            Console.WriteLine("Enter size of array");
+            size = Int32.Parse(Console.ReadLine());
+
+            int[] arr = new int[size];
+            Console.WriteLine("Enter elment of an array");
+            for(int i = 0; i < size; i++)
+            {
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(ContainsDuplicate(arr));
+        }
+    }
+}
