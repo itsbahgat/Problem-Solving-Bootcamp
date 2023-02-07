84c991ec46a2713378cb7469632fde2a5d6f594c
Sarah Ali <sara2018170167@cis.asu.edu.eg>
2023-02-07 18:43:18 +0200

    Better performance

--- Q1(stack)Valid Parentheses.cs
+++ Q1(stack)Valid Parentheses.cs
@@ -2,14 +2,14 @@ public class Solution
 {
     public bool IsValid(string s)
     {
-        Stack<char> s1 = new Stack<char>(s);
-
+        if (s.Length % 2 == 1) return false; //if size is odd => wrong
+        Stack<char> s1 = new Stack<char>(s.Length % 2); //prevent add complixity of array expansion
 
 
         foreach (char character in s)//O(n)
         {
             if (character == '(' || character == '[' || character == '{') //O(1)
-                s1.Push(character); //O(1) average  (O(n) if full)
+                s1.Push(character); //O(1) average  (O(n) if expansion)
             else
             {
                 if (s1.Count == 0) return false;// entered a closing parentheses while stack is empty  //O(1)
