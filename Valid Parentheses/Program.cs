c3c1264a19e27e5f0d6a830b9b00c64e292cc08f
Noura Mahmoud <nouramahmoud099@gmail.com>
2023-02-07 14:30:43 +0200

    solving  Longest Consecutive Sequence &  Valid Parentheses

---
+++ Valid Parentheses/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Valid_Parentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "))";
+            Console.WriteLine(IsValid(s));
+        }
+        public static bool IsValid(string s)
+        {
+            if (s.Length % 2 != 0) { return false; } // to make sure the string consists of pairs
+            Stack<char> stack = new ();
+            for(int i=0;i<s.Length;i++)
+            {
+                switch (s[i])
+                {
+                    case '(':
+                        stack.Push(s[i]);
+                        break;
+                    case '[':
+                        stack.Push(s[i]);
+                        break;
+                    case '{':
+                        stack.Push(s[i]);
+                        break;
+                    case ')': 
+                        if (stack.Count > 0)
+                        {
+                            if (stack.Peek() == '(')
+                                stack.Pop();
+                            else return false;
+                        }
+                        break;
+                    case '}':
+                        if (stack.Count > 0)
+                        {
+                            if (stack.Peek() == '{')
+                                stack.Pop();
+                            else return false;
+                        }
+                        break;
+                    case ']':
+                        if (stack.Count > 0)
+                        {
+                            if (stack.Peek() == '[')
+                                stack.Pop();
+                            else return false;
+                        }
+                        break;
+                }
+            }
+            if (stack.Count == 0) return true;
+            else
+                    return false;
+        }
+    }
+}
---
+++ t10/longestSequence/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace longestSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] nums = { 100, 4, 200, 1, 3, 2 };
+            Console.WriteLine(LongestConsecutive(nums));
+         int LongestConsecutive(int[] nums)
+        {
+                #region thought 1 time exceed
+                //var hSet = nums.ToHashSet();
+                //int seq = 0;
+                //int longest = 0;
+                //foreach(var item in hSet)
+                //{
+                //    seq = 0;
+                //    var element = item;
+                //    while(hSet.Contains(element)) 
+                //    { element += 1;
+                //        seq++;
+                //    }
+                //    longest = Math.Max(longest, seq);
+                //}
+                //return longest; 
+                #endregion
+                #region refactoring by GPT
+                var hSet = nums.ToHashSet();
+                int longest = 0;
+                foreach (var num in hSet)
+                {
+                    if (!hSet.Contains(num - 1))
+                    {
+                        int current = num;
+                        int currentSeq = 1;
+                        while (hSet.Contains(current + 1))
+                        {
+                            current++;
+                            currentSeq++;
+                        }
+                        longest = Math.Max(longest, currentSeq);
+                    }
+                }
+                return longest; 
+                #endregion
+            }
+        }
+    }
+}
